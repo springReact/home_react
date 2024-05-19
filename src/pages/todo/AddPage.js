2dc2fba3f8c9b76432718eff5447a4ee9ad8d2cb
dev-kang <kingaser@naver.com>
2024-05-19 20:11:48 +0900

    todo Read Modify Page

--- src/pages/todo/AddPage.js
+++ src/pages/todo/AddPage.js
@@ -1,9 +1,12 @@
+import {useNavigate, useParams} from "react-router-dom";
 
 const AddPage = () => {
+
   return (
     <>
       <div className={'text-3xl font-extrabold'}>
         Add Page
+
       </div>
     </>
   )
---
+++ src/pages/todo/ModifyPage.js
@@ -0,0 +1,13 @@
+
+const ModifyPage = () => {
+
+  return (
+    <>
+      <div className={'text-3xl font-extrabold'}>
+        Todo Modify Page
+      </div>
+    </>
+  )
+}
+
+export default ModifyPage;
