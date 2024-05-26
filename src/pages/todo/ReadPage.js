3de004042b25eb348a2cf097ee8fc8852e5ac3d6
dev-kang <kingaser@naver.com>
2024-05-26 17:17:34 +0900

    ListComponent

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,34 +1,15 @@
-import {createSearchParams, useNavigate, useParams, useSearchParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import ReadComponent from "../../components/todo/ReadComponent";
 
 const ReadPage = () => {
-  const navigate = useNavigate();
   const {tno} = useParams();
 
-  const [queryParams] = useSearchParams();
-
-  const page = queryParams.get('page') ? parseInt(queryParams.get('page')) : 1;
-  const size = queryParams.get('size') ? parseInt(queryParams.get('size')) : 10;
-
-  const queryStr = createSearchParams({page: page, size: size}).toString();
-
-  const moveToModify = (tno) => {
-    navigate({
-      pathname: `/todo/modify/${tno}`,
-      search: queryStr
-    })
-  }
-
-  const moveToList = (() => {
-    navigate({
-      pathname: `/todo/list`,
-      search: queryStr
-    })
-  }, [page, size])
   return (
     <>
       <div className="font-extrabold w-full bg-white mt-6">
-        <div className="text-2xl "> Todo Read Page Component {tno} </div>
+        <div className="text-2xl ">
+          Todo Read Page Component {tno}
+        </div>
         <ReadComponent tno={tno}></ReadComponent>
       </div>
     </>
